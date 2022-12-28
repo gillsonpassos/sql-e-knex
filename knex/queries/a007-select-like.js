751a2c56d7a3b5537accc2f41b4f7abc10054ede
gillsonpassos <oliveirapassos07@outlook.com>
2022-12-28 11:08:49 -0300

    Like

---
+++ knex/queries/a007-select-like.js
@@ -0,0 +1,24 @@
+/*
+-- like (parecido)
+-- % qualquer coisa
+-- _ um caractere
+select * from users
+where first_name like '%ma%_';
+*/
+const knex = require("../../knex/config/database");
+const select = knex("users")
+  .select("id", "first_name")
+  .where("first_name", "like", "___");
+
+console.log(select.toString());
+
+select
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
